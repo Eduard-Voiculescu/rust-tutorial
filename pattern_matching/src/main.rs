8e53791887c884361871b85df8721018eaa33754
Eduard Voiculescu <eduardvoiculescu95@gmail.com>
2021-12-14 18:15:00 -0500

    update

--- pattern_matching/src/main.rs
+++ pattern_matching/src/main.rs
@@ -70,6 +70,20 @@ fn main() {
         Point { x, y } => println!("On neither axis: ({}, {})", x, y),
     }
 
+    let msg = Message::ChangeColor(Color::Hsv(0, 160, 255));
+
+    match msg {
+        Message::ChangeColor(Color::Rgb(r, g, b)) => println!(
+            "Change the color to red {}, green {}, and blue {}",
+            r, g, b
+        ),
+        Message::ChangeColor(Color::Hsv(h, s, v)) => println!(
+            "Change the color to hue {}, saturation {}, and value {}",
+            h, s, v
+        ),
+        _ => (),
+    }
+
 }
 
 fn print_coordinates(&(x, y): &(i32, i32)) {
@@ -79,4 +93,16 @@ fn print_coordinates(&(x, y): &(i32, i32)) {
 struct Point {
     x: i32,
     y: i32
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(Color)
+}
+
+enum Color {
+    Rgb(i32, i32, i32),
+    Hsv(i32, i32, i32)
 }
