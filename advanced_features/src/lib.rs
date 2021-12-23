5d85463c796918b28a86d94da0e11b2ac6e11769
Eduard-Voiculescu <eduardvoiculescu95@gmail.com>
2021-12-22 20:12:59 -0500

    Adding macros

--- advanced_features/src/lib.rs
+++ advanced_features/src/lib.rs
@@ -10,3 +10,16 @@ impl Add<Meters> for Millimeters {
         Millimeters(self.0 + (rhs.0  * 1000))
     }
 }
+
+#[macro_export]
+macro_rules! vec {
+    ( $( $x:expr ),* ) => {
+        {
+            let mut temp_vec = Vec::new();
+            $(
+                temp_vec.push($x);
+            )*
+            temp_vec
+        }
+    };
+}
